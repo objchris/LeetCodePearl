4b1248509b31dcf5e7ce43be0ec384f1274fcc4e
objchris <ezfen_zhang@163.com>
2021-08-22 11:06:01 +0800

    [双指针]443压缩字符串

--- 其他/443压缩字符串/Solution.swift
+++ 其他/443压缩字符串/Solution.swift
@@ -4,3 +4,28 @@
  
  双指针求解，题目比较绕
  */
+class Solution {
+    func compress(_ chars: inout [Character]) -> Int {
+        var left = 0, right = 0, index = 0
+        let len = chars.count
+
+        while right < len {
+            while right < len && chars[index] == chars[right] {
+                right += 1
+            }
+            var count = right - index
+            chars[left] = chars[index]
+            if count != 1 {
+                let countString = String(count)
+                for c in countString {
+                    left += 1
+                    chars[left] = c
+                }
+            }
+            index = right
+            left += 1
+        }
+        return left
+    }
+
+}
