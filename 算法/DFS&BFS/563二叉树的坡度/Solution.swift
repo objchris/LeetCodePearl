97745c86993af1623b118479e34e6f514006636a
Chris Cheung <objchris@icloud.com>
2021-11-18 21:44:55 +0800

    [DFS]563. 二叉树的坡度

--- 算法/DFS&BFS/563二叉树的坡度/Solution.swift
+++ 算法/DFS&BFS/563二叉树的坡度/Solution.swift
@@ -1,3 +1,20 @@
 /*
 563. 二叉树的坡度
+https://leetcode-cn.com/problems/binary-tree-tilt/
 */
+class Solution {
+    func findTilt(_ root: TreeNode?) -> Int {
+        var ans = 0
+        dfs(root, &ans)
+        return ans
+    }
+
+    func dfs(_ root: TreeNode?, _ ans: inout Int) {
+        guard let root = root else { return }
+        dfs(root.left, &ans)
+        dfs(root.right, &ans)
+        ans += abs((root.left?.val ?? 0) - (root.right?.val ?? 0))
+        root.val += root.left?.val ?? 0
+        root.val += root.right?.val ?? 0
+    }
+}
