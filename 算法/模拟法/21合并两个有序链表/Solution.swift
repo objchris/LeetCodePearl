f7fc9035a107b8f314b0194b31f34d7365a9f510
Chris Cheung <objchris@icloud.com>
2021-12-23 23:44:10 +0800

    [递归]21. 合并两个有序链表

---
+++ 算法/模拟法/21合并两个有序链表/Solution.swift
@@ -0,0 +1,21 @@
+/*
+21. 合并两个有序链表
+https://leetcode-cn.com/problems/merge-two-sorted-lists/
+*/
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        if list1 == nil {
+            return list2
+        }
+        if list2 == nil {
+            return list1
+        }
+        if list1!.val <= list2!.val {  
+            list1?.next = mergeTwoLists(list1?.next, list2)
+            return list1
+        } else {
+            list2?.next = mergeTwoLists(list1, list2?.next) 
+            return list2
+        }
+    }
+}
