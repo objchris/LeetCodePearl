cbfc3e8ce6c0c5edd3f79c6d3356cae976b33a55
Chris Cheung <objchris@icloud.com>
2022-01-25 23:42:44 +0800

    [数学]1688. 比赛中的配对次数

---
+++ 算法/数学/1688比赛中的配对次数/Solution.swift
@@ -0,0 +1,11 @@
+/*
+1688. 比赛中的配对次数
+https://leetcode-cn.com/problems/count-of-matches-in-tournament/
+
+需要配对几次，等同于需要淘汰多少只队伍
+*/
+class Solution {
+    public int numberOfMatches(int n) {
+        return n - 1
+    }
+}
