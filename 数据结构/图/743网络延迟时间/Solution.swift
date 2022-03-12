e25a68ae6b089b93a8bfa9bb45a825f47a3d9fb8
Chris Cheung <objchris@icloud.com>
2022-03-12 17:48:10 +0800

    [dijkstra]743. 网络延迟时间

--- 数据结构/图/743网络延迟时间/Solution.swift
+++ 数据结构/图/743网络延迟时间/Solution.swift
@@ -3,36 +3,61 @@
  https://leetcode-cn.com/problems/network-delay-time/
  */
 class Solution {
+
+    let N = 110
+    let M = 6010
+    let MAX = 0x3f3f3f3f
+
+    var n: Int!
+    var k: Int!
+    var graph: [[Int]]
+    var dist: [Int]
+    var vis: [Bool]
+
+    init() {
+        graph = [[Int]](repeating: [Int](repeating: 0, count: N), count: N)
+        dist = [Int](repeating: MAX, count: N)
+        vis = [Bool](repeating: false, count: N)
+    }
+
     func networkDelayTime(_ times: [[Int]], _ n: Int, _ k: Int) -> Int {
-        // 准备工作
-        var graph = [[[Int]]](repeating: [], count: n)
-        for e in times { graph[e[0] - 1].append([e[1] - 1, e[2]]) }
-        var dist = [Int](repeating: Int.max, count: n)
-        dist[k - 1] = 0
-        var seen = [Bool](repeating: false, count: n)
-        // Dijkstra算法
-        while true {
-            // 每次扩展一个距离最短的点
-            var index = -1
-            var length = Int.max
-            for e in dist.enumerated() {
-                if !seen[e.offset] && dist[e.offset] < length {
-                    length = dist[e.offset]
-                    index = e.offset
-                }
-            }
-            // 更新与其相邻点的距离
-            if index != -1 {
-                seen[index] = true
-                for e in graph[index] ?? [] {
-                    dist[e[0]] = min(dist[e[0]], dist[index] + e[1])
-                }
-            } else {
-                break
+        self.n = n
+        self.k = k
+        // 初始化
+        for i in 1...n {
+            for j in 1...n {
+                graph[i][j] = (i == j) ? 0 : MAX
+                graph[j][i] = (i == j) ? 0 : MAX
             }
         }
-        // 返回
-        let ans = dist.max()!
-        return ans == Int.max ? -1 : ans
+
+        for t in times {
+            let a = t[0], b = t[1], w = t[2]
+            graph[a][b] = w
+        }
+
+        dijkstra(k)
+
+        var ans = 0
+        for i in 1...n {
+            ans = max(ans, dist[i])
+        }
+        return ans == MAX ? -1 : ans
+    }
+
+    func dijkstra(_ start: Int) {
+        dist[start] = 0
+        for _ in 1...n { 
+            // 每次都会遍历一个点，所以共需要遍历 n 次
+            var t = -1
+            for i in 1...n where !vis[i] { 
+                // 每次找「未遍历过」且「最短距离最小」的点
+                if t == -1 || dist[i] < dist[t] { t = i }
+            }
+            vis[t] = true
+            for i in 1...n { // 用点 t 的「最小距离」更新其他点
+                dist[i] = min(dist[i], dist[t] + graph[t][i])
+            }
+        }
     }
 }
